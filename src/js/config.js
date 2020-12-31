0396782eb2eb658d5c7dfd3775b96f9f5ff80c19
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-12-31 08:59:12 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6df067a2-998f-43c5-a625-6727e9f4a363';
+export const MODAL_CLOSE_SEC = 2.5;
